f858a65aed5222ff8dd95375a0ae49446071476c
Phat Thaveepholcharoen <overtone00445@gmail.com>
2021-12-01 20:25:52 +0700

    Implement gcd

--- algo/greatestCommomDiviser/main.py
+++ algo/greatestCommomDiviser/main.py
@@ -1,26 +1,12 @@
 
-def greatestCommomDiviser(array):
-    div = 2
-    count = 0
-
-    for num in array:
-        if num % div != 0:
-            div += 1
-
-        if num % div == 0:
-            count += 1
-
-        if count == len(array):
-            break
-
-    if count < len(array):
-        div = 1
+def greatestCommomDiviser(a, b):
+    if b == 0:
+        return a
+    return greatestCommomDiviser(b, a % b)
 
 
 if __name__ == '__main__':
-    r1 = greatestCommomDiviser([4, 6, 8])
-    r2 = greatestCommomDiviser([3, 6, 9])
-    r3 = greatestCommomDiviser([18, 45])
+    r1 = greatestCommomDiviser(4, 8)
+    r2 = greatestCommomDiviser(2, 3)
     print(r1)
     print(r2)
-    print(r3)
